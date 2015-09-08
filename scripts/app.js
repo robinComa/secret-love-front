6fdc8625e0a77b1451b90f523333f1df0b10a0c6
Robin Coma Delperier <rcomadelperier@sqli.com>
2015-09-08 23:08:52 +0200

    Update 2015-09-08T21:08:45.053Z

--- scripts/app.js
+++ scripts/app.js
@@ -512,6 +512,41 @@ angular.module('app').provider('Language', function(){
 });
 'use strict';
 
+angular.module('app').run(function ($log, $window) {
+
+    if($window.applicationCache){
+        // Enable new cache when available and reload page
+        $window.applicationCache.addEventListener('updateready', function () {
+            $window.applicationCache.swapCache();
+            $log.info('Cache updating');
+            $window.location.reload();
+        }, false);
+
+        // Cache in error
+        $window.applicationCache.addEventListener('error', function () {
+            $log.info('Cache in error : ');
+        }, false);
+
+        // Check of the manifest release
+        $window.applicationCache.addEventListener('checking', function () {
+            $log.info('Cache checking');
+        }, false);
+
+        // Manifest changed : download the new content
+        $window.applicationCache.addEventListener('obsolete', function () {
+            $log.info('Cache not up-to-date');
+            $window.applicationCache.update();
+        }, false);
+
+        // Manifest not changed
+        $window.applicationCache.addEventListener('noupdate', function () {
+            $log.info('Manifest not changed');
+        }, false);
+    }
+
+});
+'use strict';
+
 angular.module('app').directive('basketContent', function($timeout){
     return {
         restrict: 'E',
