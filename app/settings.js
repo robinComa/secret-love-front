e44c34695cd6b58e4ccb6ab02886150bf68a407f
robinComa <robin.coma.public@gmail.com>
2015-10-09 00:04:13 +0200

    endpoint amazon

--- app/settings.js
+++ app/settings.js
@@ -4,7 +4,7 @@
     var origin = window.location.href.split(window.location.hash)[0];
 
     angular.module('app').constant('settings', {
-        endpoint: 'http://localhost:9001/rest-api/',
+        endpoint: 'http://secret-love-back-dev.elasticbeanstalk.com/',
         toast: {
             hideDelay: 5000,
             position: 'bottom left'
